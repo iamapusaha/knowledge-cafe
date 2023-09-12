a6e7d689f44c40ca0372f8c12462434c99e9d532
iamapusaha <apusaha392@gmail.com>
2023-09-12 11:26:02 +0600

    add hero icon

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { BookmarkIcon } from '@heroicons/react/24/solid'
 
 const Blog = ({ blog }) => {
     const { title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
@@ -10,11 +11,12 @@ const Blog = ({ blog }) => {
                     <img className='w-14 ' src={author_img} alt="" />
                     <div>
                         <h3 className='text-2xl font-bold'>{author}</h3>
-                        <p className='text-base font-semibold'>{posted_date}</p>
+                        <p className='text-base font-semibold text-[#11111199]'>{posted_date}</p>
                     </div>
                 </div>
-                <div>
+                <div className='flex gap-1 text-xl font-semibold text-[#11111199]'>
                     <p>{reading_time} min read</p>
+                    <button><BookmarkIcon className="h-6 w-6"></BookmarkIcon></button>
                 </div>
             </div>
             <p className='text-4xl font-bold my-4'>{title}</p>
